1806dd84b19de5de3f3c0e3548b8eeafda75b95b
Baiteli <toplegionmainer@gmail.com>
2026-05-08 01:34:41 +0600

    feat: add ingredients menu and types, constant data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import './App.css'
+import "./App.css";
+import type { Ingredient, IngredientCount } from "./type";
+
+import BaconImg from "./assets/bacon.jpg";
+import CheeseImg from "./assets/cheese.jpg";
+import MeatImg from "./assets/meat.jpg";
+import SaladImg from "./assets/salad.jpg";
+
+const INGREDIENTS: Ingredient[] = [
+  { name: "bacon", price: 60, image: BaconImg },
+  { name: "cheese", price: 50, image: CheeseImg },
+  { name: "meat", price: 80, image: MeatImg },
+  { name: "salad", price: 10, image: SaladImg },
+];
 
 const App = () => {
-  return (
-    <>
-    </>
-  )
-}
+  return <></>;
+};
 
-export default App
+export default App;
---
+++ src/type.ts
@@ -0,0 +1,10 @@
+export interface Ingredient {
+  name: string;
+  price: number;
+  image: string;
+}
+
+export interface IngredientCount {
+  name: string;
+  count: number;
+}
