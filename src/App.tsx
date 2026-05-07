2457ada02d2b5a8c70c9998a128e3f8709a0fe03
Baiteli <toplegionmainer@gmail.com>
2026-05-08 03:57:47 +0600

    feat: complete burger logic and price calculation

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import BaconImg from "./assets/bacon.jpg";
 import CheeseImg from "./assets/cheese.jpg";
 import MeatImg from "./assets/meat.jpg";
 import SaladImg from "./assets/salad.jpg";
+import { useState } from "react";
 
 const INGREDIENTS: Ingredient[] = [
   { name: "bacon", price: 60, image: BaconImg },
@@ -14,6 +15,41 @@ const INGREDIENTS: Ingredient[] = [
 ];
 
 const App = () => {
+  const [ingredients, setIngredients] = useState<IngredientCount[]>([
+    { name: "bacon", count: 0 },
+    { name: "cheese", count: 0 },
+    { name: "meat", count: 0 },
+    { name: "salad", count: 0 },
+  ]);
+
+  const addIngredient = (userIngredient: string) => {
+    setIngredients(
+      ingredients.map((item) =>
+        item.name === userIngredient
+          ? { ...item, count: item.count + 1 }
+          : item,
+      ),
+    );
+  };
+
+  const removeIngredient = (userRemoved: string) => {
+    setIngredients(
+      ingredients.map((item) =>
+        item.name === userRemoved
+          ? item.count > 0
+            ? { ...item, count: item.count - 1 }
+            : item
+          : item,
+      ),
+    );
+  };
+
+  const totalPrice = ingredients.reduce((acc, item) => {
+    const currentIngredient = INGREDIENTS.find((ing) => ing.name === item.name);
+    if (!currentIngredient) return acc;
+    return acc + currentIngredient.price * item.count;
+  }, 30);
+
   return <></>;
 };
 
